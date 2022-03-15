e9a40d708e4115914737721103e556799bf25020
Sabiduria10 <oyinseyeshola@gmail.com>
2022-03-15 18:46:20 +0000

    The function that the life of jack bauer from 00:00 to 23:59

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,10 +16,9 @@ void jack_bauer(void)
 		j = 0;
 		while (j <= 59)
 		{
-			
 			_putchar((x / 10) + '0');
 			_putchar((x % 10) + '0');
-			_putchar((':');
+			_putchar(':');
 			_putchar((j / 10) + '0');
 			_putchar((j % 10) + '0');
 			_putchar('\n');
