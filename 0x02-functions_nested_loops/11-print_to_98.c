5278dd0f186d79a8e2a0a36339299d229bd999f7
Sabiduria10 <oyinseyeshola@gmail.com>
2022-03-15 19:25:10 +0000

    prints all natural numbers from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,7 +15,7 @@ void print_to_98(int n)
 		{
 			printf("%d, ", n);
 		}
-	{
+	}
 	else if (n > 98)
 	{
 		for (; n > 98; n--)
@@ -28,4 +28,3 @@ void print_to_98(int n)
 		printf("%d\n", n);
 	}
 }
-
