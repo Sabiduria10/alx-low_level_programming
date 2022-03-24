6257582c47a97e5af1088127f9f4aa6ab25bf94c
Sabiduria10 <oyinseyeshola@gmail.com>
2022-03-24 13:45:02 +0000

    Pointers arrays and strings

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
-#define ROT13IN "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-#define ROTIN13OUT "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"
+#define ROT13IN  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+#define ROT13OUT "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"
 
 /**
- * rot13 - encode a string in rot13
+ * rot13 - encodes a string in rot13
  * @s: the string to encode
  *
  * Return: char pointer
@@ -14,9 +14,9 @@ char *rot13(char *s)
 	char *ret = s, *rot13in = ROT13IN, *rot13out = ROT13OUT;
 	int i = 0;
 
-	for (; *s, s++)
+	for (; *s; s++)
 	{
-		for(i = 0; rot13in[i]; i++)
+		for (i = 0; rot13in[i]; i++)
 			if (*s == rot13in[i])
 			{
 				*s = rot13out[i];
@@ -25,4 +25,3 @@ char *rot13(char *s)
 	}
 	return (ret);
 }
-
