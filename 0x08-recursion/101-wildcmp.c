2920c2995339fc134daa33b174ee7c0647f902ff
Sabiduria10 <oyinseyeshola@gmail.com>
2022-03-30 21:31:20 +0000

    Recursions

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -13,7 +13,7 @@ int wildcmp(char *s1, char *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 	if (*s2 == '*')
 	{
-		if (*(s2 + 1) + == '*')
+		if (*(s2 + 1) == '*')
 			return (wildcmp(s1, s2 + 1));
 		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
 			return (1);
