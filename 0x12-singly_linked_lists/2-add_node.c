6701f1a384258e465bc4e782c7c218b12fdb170c
Sabiduria10 <oyinseyeshola@gmail.com>
2022-04-22 00:53:35 +0000

    Single linked listed

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,49 +3,26 @@
 #include <string.h>
 
 /**
- * add_node_end - Add a new element to a list_t list at the end
- * @head: Pointer to pointer to start of the list
- * @str: String to copy to str element of list_t item
+ * add_node - Add an element to a list at the front of the list
+ * @head: Pointer to the pointer to the start of the list
+ * @str: Pointer to the string to be put into the element of the list
  *
- * Return: Address of new element, NULL if fails
+ * Return: Address of new element, or NULL if it failed
  */
-list_t *add_node_end(list_t **head, const char *str)
+list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
-	list_t *current;
 	unsigned int i;
 
 	i = 0;
-	if (*head == NULL)
-	{
-		new = malloc(sizeof(list_t));
-		if (new == NULL)
-			return (NULL);
-		new->next = NULL;
-		*head = new;
-		new->str = strdup(str);
-		while (str[i] != '\0')
-			i++;
-		new->len = i;
-	}
-	else
-	{
-		current = *head;
-		while (1)
-		{
-			if (current->next == NULL)
-				break;
-			current = current->next;
-		}
-		new = malloc(sizeof(list_t));
-		if (new == NULL)
-			return (NULL);
-		new->next = NULL;
-		current->next = new;
-		new->str = strdup(str);
-		while (str[i] != '\0')
-			i++;
-		new->len = i;
-	}
-	return (new);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	while (str[i] != '\0')
+		i++;
+	new->len = i;
+	new->next = *head;
+	*head = new;
+	return (*head);
 }
