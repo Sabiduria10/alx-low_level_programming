bd5d512868b98c893c536481d22c684e9e4dbd02
Sabiduria10 <oyinseyeshola@gmail.com>
2022-05-03 13:03:32 +0000

    More single linked lists in C

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,5 +6,6 @@ int clear_bit(unsigned long int *n, unsigned int index);
 int _putchar(char c);
 int set_bit(unsigned long int *n, unsigned int index);
 int get_bit(unsigned long int n, unsigned int index);
-void int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+unsigned int binary_to_uint(const char *b);
 #endif
