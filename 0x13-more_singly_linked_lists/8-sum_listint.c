1676706b567d9c992e87b0bc674c9c0bfbb757c1
Sabiduria10 <oyinseyeshola@gmail.com>
2022-05-03 11:52:48 +0000

    More single linked lists in C

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,22 +1,23 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * sum_listint - returns the sum of all the data (n) of
- * a linked list.
- * @head: head of a list.
+ * sum_listint - Add up all the data, integers, from a list
+ * @head: Pointer to the start of the list
  *
- * Return: sum of all the data (n).
+ * Return: The sum of all data, 0 if the list is empty
  */
 int sum_listint(listint_t *head)
 {
 	int sum;
 
 	sum = 0;
+	if (head == NULL)
+		return (0);
 	while (head != NULL)
 	{
 		sum += head->n;
 		head = head->next;
 	}
-
 	return (sum);
 }
